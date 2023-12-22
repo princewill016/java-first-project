98a7d9c8863c90cf78ed36803d9ca1df49c00ed2
Peter David <Princewill016@gmail.com>
2023-12-22 21:02:56 +0100

    added favourite food

--- App.java
+++ App.java
@@ -5,12 +5,15 @@ public class App {
         try (Scanner scanner = new Scanner(System.in)) {
             System.out.println("what is your name??");
             String userName = scanner.nextLine();
-            System.out.println("welcome " + userName);
             System.out.println("How old are you??");
             int userAge = scanner.nextInt();
-            System.out.println("you are " + userAge + " years old!");
+            scanner.nextLine();
+            System.out.println("what is your favourite food???");
+            String food = scanner.nextLine();
+            System.out.println("welcome " + userName);
+            System.out.println("you are " + userAge + "years old!");
+            System.out.println(" and your favourite food is " + food);
         }
     }
 
-
-}
+};
