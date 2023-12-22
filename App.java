fd710c7c02a5872de332795f3fee37869386e7d3
Peter David <Princewill016@gmail.com>
2023-12-22 20:45:36 +0100

    testing the scanner

--- App.java
+++ App.java
@@ -1,8 +1,16 @@
-public class App{
- public static void main(String[] args) {
-    
-    
- }
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) {
+        try (Scanner scanner = new Scanner(System.in)) {
+            System.out.println("what is your name??");
+            String userName = scanner.nextLine();
+            System.out.println("welcome " + userName);
+            System.out.println("How old are you??");
+            int userAge = scanner.nextInt();
+            System.out.println("you are " + userAge + " years old!");
+        }
+    }
 
 
 }
