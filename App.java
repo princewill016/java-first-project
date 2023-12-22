2b6be1a7bffa929335ba9fde3d6c922eaada2693
Peter David <Princewill016@gmail.com>
2023-12-23 00:19:42 +0100

    learnt random number

--- App.java
+++ App.java
@@ -1,19 +1,45 @@
-import java.util.Scanner;
+
+// import java.util.Scanner;
+// import javax.swing.JOptionPane;
+// import java.util.Random;
 
 public class App {
     public static void main(String[] args) {
-        try (Scanner scanner = new Scanner(System.in)) {
-            System.out.println("what is your name??");
-            String userName = scanner.nextLine();
-            System.out.println("How old are you??");
-            int userAge = scanner.nextInt();
-            scanner.nextLine();
-            System.out.println("what is your favourite food???");
-            String food = scanner.nextLine();
-            System.out.println("welcome " + userName);
-            System.out.println("you are " + userAge + "years old!");
-            System.out.println(" and your favourite food is " + food);
-        }
-    }
 
-};
+    }
+}
+
+/*
+ * ENTER TEXT IN THE TERMINAL
+ * try (Scanner scanner = new Scanner(System.in)) {
+ * System.out.println("what is your name??");
+ * String userName = scanner.nextLine();
+ * System.out.println("How old are you??");
+ * int userAge = scanner.nextInt();
+ * scanner.nextLine();
+ * System.out.println("what is your favourite food???");
+ * String food = scanner.nextLine();
+ * System.out.println("welcome " + userName);
+ * System.out.println("you are " + userAge + "years old!");
+ * System.out.println(" and your favourite food is " + food);
+ * 
+ * 
+ * GUI{GRAPHICAL USER INTERPHASE}
+ * String dialog = JOptionPane.showInputDialog("Tell us your name!");
+ * JOptionPane.showMessageDialog(null, "Hello, " + dialog);
+ * int age =
+ * Integer.parseInt(JOptionPane.showInputDialog("Tell us your age !"));
+ * JOptionPane.showMessageDialog(null, "You are " + age + " years old!");
+ * float height =
+ * Float.parseFloat(JOptionPane.showInputDialog("Tell us your height !"));
+ * JOptionPane.showMessageDialog(null, "You are " + height + "cm");
+ * 
+ * GENERATE RANDOM NUMBER BETWEEN 1 AND 6
+ * Random number = new Random();
+ * int x = number.nextInt(6) + 1;
+ * System.out.println(x);
+ * 
+ * 
+ * 
+ * 
+ */
